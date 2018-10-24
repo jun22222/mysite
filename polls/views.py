5cd0fdb4beac63c4e5ededb1b0762c1fa476e460
HPE <injh9900@naver.com>
2018-10-24 17:03:01 +0900

    투표페이지??

---
+++ polls/views.py
@@ -0,0 +1,62 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from polls.models import Question, Choice
+from django.utils import timezone
+
+def input(request):
+    return render(request, 'polls/input.html', {})
+
+
+
+
+
+
+def puut(request, id):
+    que = Question.objects.get(id = id)
+    return render(request, '', {})
+
+
+def add_question(request):
+    text = request.POST['text']
+    q = Question(
+        question_text = text,
+        pub_date=timezone.now())
+    q.save()
+    return render(request, 'polls/add.html', {})
+    return HttpResponse('입력완료')
+
+
+
+def data(request):     #<!--데이터가 넘어간디야~~~-->
+   #http://localhost:8000/polls/data?user_name=kim (GET방식)
+    value = request.GET['user_name']
+
+    return HttpResponse(value)
+
+def vote(request):
+    choice = request.POST['choice']
+    c = Choice.objects.get(pk=choice)
+    c.votes = c.votes + 1
+    c.save()
+
+    return render(
+        request,
+        'polls/vote.html',
+        {}
+    )
+
+def index(request):
+    list = Question.objects.all()
+
+    return render(
+        request, 'polls/index.html',
+        {'question' : list})
+        #넘길때 이름(키) : 내용
+
+          # 템플릿을 부르면서 질문을 줘야함
+# Create your views here.
+
+# polls의 url
+def detail(request, id):
+    question = Question.objects.get(id=id)
+    return render(request, 'polls/detail.html', {'item' : question})
